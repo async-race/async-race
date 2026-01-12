6692c46f7b6b32c491083302ca6aece1fa9ee276
Elena Khadasevich <camatsu90@mail.ru>
2026-01-12 13:18:54 +0300

    feat: add Button component with tests and install incoming dependencies

---
+++ src/app/shared/ui/button/button.test.ts
@@ -0,0 +1,39 @@
+import { Button } from './button';
+import { describe, expect, it, vi } from 'vitest';
+
+describe('Button component', () => {
+  it('creates an instance of button with given text', () => {
+    const btn = Button('Press me', false, vi.fn());
+
+    expect(btn.tagName).toBe('BUTTON');
+    expect(btn.textContent).toBe('Press me');
+  });
+
+  it('adds base Classes', () => {
+    const btn = Button('Press me', false, vi.fn());
+
+    expect(btn.className).toContain('bg-sky-400');
+    expect(btn.className).toContain('rounded-2xl');
+  });
+
+  it('adds extra Classes', () => {
+    const btn = Button('Press me', false, vi.fn(), 'shadow-lg');
+
+    expect(btn.className).toContain('shadow-lg');
+  });
+
+  it('adds disabled', () => {
+    const btn = Button('Press me', true, vi.fn());
+
+    expect(btn.disabled).toBe(true);
+  });
+
+  it('calls onClick callback function', () => {
+    const onClick = vi.fn();
+    const btn = Button('Press me', false, onClick);
+
+    btn.click();
+
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ src/app/shared/ui/button/button.ts
@@ -0,0 +1,24 @@
+export function Button(
+  textContent: string,
+  disabled: boolean,
+  onClick: (event: MouseEvent | KeyboardEvent) => void,
+  extraClasses?: string,
+): HTMLButtonElement {
+  const baseClasses = `
+    bg-sky-400 hover:bg-sky-700 
+    p-2 
+    w-18 sm:w-28 md:w-34
+    text-base sm:text-lg md:text-xl 
+    text-white 
+    rounded-2xl transition-colors duration-300
+    flex items-center justify-center
+  `;
+
+  const button = document.createElement('button');
+  button.className = `${baseClasses} ${extraClasses}`.trim();
+  button.textContent = textContent;
+  button.disabled = disabled;
+  button.addEventListener('click', onClick);
+
+  return button;
+}
