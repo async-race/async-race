821f1cff3c68a1eba07b19f74c8e0608c6a68ce7
Elena Khadasevich <camatsu90@mail.ru>
2026-01-13 13:37:15 +0300

    feat: add Link component with tests

---
+++ src/app/shared/ui/link/Link.test.ts
@@ -0,0 +1,34 @@
+import { Link } from './Link';
+import { describe, expect, it } from 'vitest';
+
+describe('Link component', () => {
+  it('create an instance of link with given url', () => {
+    const link = Link({ href: '/home' });
+
+    expect(link.href).toContain('/home');
+  });
+
+  it('adds target attribute', () => {
+    const link = Link({ href: '/home', target: '_blank' });
+
+    expect(link.target).toContain('_blank');
+  });
+
+  it('adds base Classes', () => {
+    const link = Link({ href: '/home' });
+
+    expect(link.className).toContain('text-white');
+  });
+
+  it('adds extra Classes', () => {
+    const link = Link({ href: '/home', extraClasses: 'custom' });
+
+    expect(link.className).toContain('custom');
+  });
+
+  it('adds title attribute', () => {
+    const link = Link({ href: '/home', title: 'navigate to home page' });
+
+    expect(link.title).toContain('navigate to home page');
+  });
+});
