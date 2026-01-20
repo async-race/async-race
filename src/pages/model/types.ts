07324970a56664d7da44246462617e88ba6369c8
Elena Khadasevich <camatsu90@mail.ru>
2026-01-20 10:52:29 +0300

    feat: add type WinnersWithCars for winnerTable

--- src/pages/model/types.ts
+++ src/pages/model/types.ts
@@ -1,5 +1,10 @@
+import type { Car } from '@/entities/car/model/types';
+import type { Winner } from '@/entities';
+
 export type PageInstance = {
   element: HTMLElement;
   show: () => void;
   hide: () => void;
 };
+
+export type WinnersWithCars = Winner & Pick<Car, 'name' | 'color'>;
