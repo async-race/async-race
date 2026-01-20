835d846fb54e12667843203da2af831f0011f3b0
Elena Khadasevich <camatsu90@mail.ru>
2026-01-20 16:11:17 +0300

    test: add new test to BlockComponent

--- src/shared/ui/block-component/block-component.test.ts
+++ src/shared/ui/block-component/block-component.test.ts
@@ -1,22 +1,19 @@
 import { BlockComponent } from './block-component';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 
 describe('BlockComponent', () => {
   it('creates an instance of BlockComponent with given tag', () => {
     const element = BlockComponent({ tagName: 'div' });
-
     expect(element.tagName.toLowerCase()).toBe('div');
   });
 
   it('adds base style', () => {
     const element = BlockComponent({ tagName: 'h1' });
-
     expect(element.className).toContain('md:text-4xl');
   });
 
   it('adds extraClasses', () => {
     const element = BlockComponent({ tagName: 'p', extraClasses: 'custom' });
-
     expect(element.className).toContain('custom');
   });
 
@@ -25,7 +22,18 @@ describe('BlockComponent', () => {
       tagName: 'h2',
       textContent: 'async-race',
     });
-
     expect(element.textContent).toBe('async-race');
+  });
+
+  it('calls onClick handler when clicked', () => {
+    const handleClick = vi.fn();
+    const element = BlockComponent({
+      tagName: 'button',
+      textContent: 'Click me',
+      onClick: handleClick,
+    });
+
+    element.click();
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
