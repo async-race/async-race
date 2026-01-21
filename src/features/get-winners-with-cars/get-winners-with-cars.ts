7ab74f064248cee3eac2cb1b22701c832ecb00e6
Elena Khadasevich <camatsu90@mail.ru>
2026-01-21 16:00:52 +0300

    feat: add getWinnersWithCars function with tests

--- src/features/get-winners-with-cars/get-winners-with-cars.ts
+++ src/features/get-winners-with-cars/get-winners-with-cars.ts
@@ -1,22 +1,26 @@
-//import { getCars } from '@/entities/car/api/car.api';
 import type { Winner } from '@/entities/winner/model/types';
 import type { Car } from '@/entities/car/model/types';
-import { getCars } from '@/entities/car/api/car.api';
+import { getCar } from '@/entities/car/api/car.api';
 
 export type WinnersWithCars = Winner & Pick<Car, 'name' | 'color'>;
 
-export async function getWinnersWithCars(
+export function getWinnersWithCars(
   winners: Winner[],
 ): Promise<WinnersWithCars[]> {
-  const { items: cars } = await getCars();
-
-  const winnersAndCars = winners.map((winner: Winner) => {
-    const car = cars.find((item: Car) => item.id === winner.id);
-    return {
-      ...winner,
-      name: car?.name ?? '',
-      color: car?.color ?? '',
-    };
-  });
-  return winnersAndCars;
+  const winnersIds = winners.map((winner) => winner.id);
+  const cars = winnersIds.map((id) => getCar(id));
+  return Promise.all(cars)
+    .then((responses) => {
+      return winners.map((winner: Winner) => {
+        const car = responses.find((value) => value.data.id === winner.id);
+        return {
+          ...winner,
+          name: car?.data.name ?? '',
+          color: car?.data.color ?? '',
+        };
+      });
+    })
+    .catch(() => {
+      throw new Error("Winners aren't fetched!");
+    });
 }
