28bc9eaae0fb57072e11cd5e67c3bd40d5c2ef3a
Elena Khadasevich <camatsu90@mail.ru>
2026-01-22 17:53:51 +0300

    feat: add generateCars function

---
+++ src/features/generate-cars/generate-cars.ts
@@ -0,0 +1,28 @@
+import { getRandomColor } from './lib/random-color/random-color';
+import { getRandomInt } from './lib/random-number/random-number';
+import type { Car } from '@/entities';
+import { randomCar } from './model/types';
+import { createCar } from '@/entities/car/api/car.api';
+import { carsStore } from '@/entities';
+
+export async function generateCars(): Promise<Car[]> {
+  const carsRandomCollection: Car[] = [];
+  for (let index = 0; index < 100; index += 1) {
+    const carIndex = getRandomInt(0, randomCar.length - 1);
+    const color = getRandomColor();
+    try {
+      const { data } = await createCar({
+        name: randomCar[carIndex],
+        color: color,
+      });
+
+      carsRandomCollection.push(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  carsStore.set(carsRandomCollection, carsRandomCollection.length);
+
+  return carsRandomCollection;
+}
