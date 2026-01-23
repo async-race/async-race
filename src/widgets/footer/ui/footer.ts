94c89dafc4316cd573f3b8ea469697e89e80b39b
Elena Khadasevich <camatsu90@mail.ru>
2026-01-24 00:19:54 +0300

    feat: add styles and link to footer

--- src/widgets/footer/ui/footer.ts
+++ src/widgets/footer/ui/footer.ts
@@ -2,10 +2,14 @@ import { BlockComponent } from '@/shared/ui/block-component/block-component';
 
 export function createFooter() {
   const footer = BlockComponent({
-    tagName: 'div',
-    extraClasses: 'flex gap-4',
-    textContent: '2026',
+    tagName: 'p',
+    extraClasses: 'font-bold p-1 pl-4',
   });
+  const link = document.createElement('a');
+  link.href = 'https://github.com/async-race/async-race';
+  link.textContent = 'Developed by Team .by in 2026';
+  link.className = 'text-blue-600 hover:underline';
+  footer.append(link);
 
   return footer;
 }
